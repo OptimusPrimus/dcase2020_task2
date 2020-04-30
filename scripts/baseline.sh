758a90f703e2287f96f72179fed9ea39ed8d503c
Paul Primus <primus_paul@yahoo.de>
2020-04-30 11:27:02 +0200

    fix critical bug in validation

--- scripts/baseline.sh
+++ scripts/baseline.sh
@@ -1,33 +1,33 @@
 cd ..
-cd dcase2020_task2
-conda activate dcase2020_task2
+cd dcase2020_2
+conda activate dcase2020_2
 
-OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=$3 python -m experiments.baseline_experiment with num_workers=4 machine_type=0 machine_id=0 normalize=$1 normalize_raw=$2 -m student2.cp.jku.at:27017:dcase2020_task2
-OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=$3 python -m experiments.baseline_experiment with num_workers=4 machine_type=0 machine_id=2 normalize=$1 normalize_raw=$2 -m student2.cp.jku.at:27017:dcase2020_task2
-OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=$3 python -m experiments.baseline_experiment with num_workers=4 machine_type=0 machine_id=4 normalize=$1 normalize_raw=$2 -m student2.cp.jku.at:27017:dcase2020_task2
-OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=$3 python -m experiments.baseline_experiment with num_workers=4 machine_type=0 machine_id=6 normalize=$1 normalize_raw=$2 -m student2.cp.jku.at:27017:dcase2020_task2
 
-OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=$3 python -m experiments.baseline_experiment with num_workers=4 machine_type=1 machine_id=0 normalize=$1 normalize_raw=$2 -m student2.cp.jku.at:27017:dcase2020_task2
-OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=$3 python -m experiments.baseline_experiment with num_workers=4 machine_type=1 machine_id=2 normalize=$1 normalize_raw=$2 -m student2.cp.jku.at:27017:dcase2020_task2
-OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=$3 python -m experiments.baseline_experiment with num_workers=4 machine_type=1 machine_id=4 normalize=$1 normalize_raw=$2 -m student2.cp.jku.at:27017:dcase2020_task2
-OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=$3 python -m experiments.baseline_experiment with num_workers=4 machine_type=1 machine_id=6 normalize=$1 normalize_raw=$2 -m student2.cp.jku.at:27017:dcase2020_task2
+OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=$2 python -m experiments.baseline_experiment with num_workers=4 machine_type=0 machine_id=0 descriptor=$1 -m student2.cp.jku.at:27017:dcase2020_2
+OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=$2 python -m experiments.baseline_experiment with num_workers=4 machine_type=0 machine_id=2 descriptor=$1 -m student2.cp.jku.at:27017:dcase2020_2
+OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=$2 python -m experiments.baseline_experiment with num_workers=4 machine_type=0 machine_id=4 descriptor=$1 -m student2.cp.jku.at:27017:dcase2020_2
+OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=$2 python -m experiments.baseline_experiment with num_workers=4 machine_type=0 machine_id=6 descriptor=$1 -m student2.cp.jku.at:27017:dcase2020_2
 
-OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=$3 python -m experiments.baseline_experiment with num_workers=4 machine_type=2 machine_id=0 normalize=$1 normalize_raw=$2 -m student2.cp.jku.at:27017:dcase2020_task2
-OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=$3 python -m experiments.baseline_experiment with num_workers=4 machine_type=2 machine_id=2 normalize=$1 normalize_raw=$2 -m student2.cp.jku.at:27017:dcase2020_task2
-OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=$3 python -m experiments.baseline_experiment with num_workers=4 machine_type=2 machine_id=4 normalize=$1 normalize_raw=$2 -m student2.cp.jku.at:27017:dcase2020_task2
-OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=$3 python -m experiments.baseline_experiment with num_workers=4 machine_type=2 machine_id=6 normalize=$1 normalize_raw=$2 -m student2.cp.jku.at:27017:dcase2020_task2
+OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=$2 python -m experiments.baseline_experiment with num_workers=4 machine_type=1 machine_id=0 descriptor=$1 -m student2.cp.jku.at:27017:dcase2020_2
+OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=$2 python -m experiments.baseline_experiment with num_workers=4 machine_type=1 machine_id=2 descriptor=$1 -m student2.cp.jku.at:27017:dcase2020_2
+OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=$2 python -m experiments.baseline_experiment with num_workers=4 machine_type=1 machine_id=4 descriptor=$1 -m student2.cp.jku.at:27017:dcase2020_2
+OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=$2 python -m experiments.baseline_experiment with num_workers=4 machine_type=1 machine_id=6 descriptor=$1 -m student2.cp.jku.at:27017:dcase2020_2
 
-OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=$3 python -m experiments.baseline_experiment with num_workers=4 machine_type=3 machine_id=1 normalize=$1 normalize_raw=$2 -m student2.cp.jku.at:27017:dcase2020_task2
-OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=$3 python -m experiments.baseline_experiment with num_workers=4 machine_type=3 machine_id=2 normalize=$1 normalize_raw=$2 -m student2.cp.jku.at:27017:dcase2020_task2
-OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=$3 python -m experiments.baseline_experiment with num_workers=4 machine_type=3 machine_id=3 normalize=$1 normalize_raw=$2 -m student2.cp.jku.at:27017:dcase2020_task2
-OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=$3 python -m experiments.baseline_experiment with num_workers=4 machine_type=3 machine_id=4 normalize=$1 normalize_raw=$2 -m student2.cp.jku.at:27017:dcase2020_task2
+OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=$2 python -m experiments.baseline_experiment with num_workers=4 machine_type=2 machine_id=0 descriptor=$1 -m student2.cp.jku.at:27017:dcase2020_2
+OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=$2 python -m experiments.baseline_experiment with num_workers=4 machine_type=2 machine_id=2 descriptor=$1 -m student2.cp.jku.at:27017:dcase2020_2
+OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=$2 python -m experiments.baseline_experiment with num_workers=4 machine_type=2 machine_id=4 descriptor=$1 -m student2.cp.jku.at:27017:dcase2020_2
+OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=$2 python -m experiments.baseline_experiment with num_workers=4 machine_type=2 machine_id=6 descriptor=$1 -m student2.cp.jku.at:27017:dcase2020_2
 
-OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=$3 python -m experiments.baseline_experiment with num_workers=4 machine_type=4 machine_id=1 normalize=$1 normalize_raw=$2 -m student2.cp.jku.at:27017:dcase2020_task2
-OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=$3 python -m experiments.baseline_experiment with num_workers=4 machine_type=4 machine_id=2 normalize=$1 normalize_raw=$2 -m student2.cp.jku.at:27017:dcase2020_task2
-OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=$3 python -m experiments.baseline_experiment with num_workers=4 machine_type=4 machine_id=3 normalize=$1 normalize_raw=$2 -m student2.cp.jku.at:27017:dcase2020_task2
+OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=$2 python -m experiments.baseline_experiment with num_workers=4 machine_type=3 machine_id=1 descriptor=$1 -m student2.cp.jku.at:27017:dcase2020_2
+OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=$2 python -m experiments.baseline_experiment with num_workers=4 machine_type=3 machine_id=2 descriptor=$1 -m student2.cp.jku.at:27017:dcase2020_2
+OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=$2 python -m experiments.baseline_experiment with num_workers=4 machine_type=3 machine_id=3 descriptor=$1 -m student2.cp.jku.at:27017:dcase2020_2
+OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=$2 python -m experiments.baseline_experiment with num_workers=4 machine_type=3 machine_id=4 descriptor=$1 -m student2.cp.jku.at:27017:dcase2020_2
 
-OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=$3 python -m experiments.baseline_experiment with num_workers=4 machine_type=5 machine_id=0 normalize=$1 normalize_raw=$2 -m student2.cp.jku.at:27017:dcase2020_task2
-OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=$3 python -m experiments.baseline_experiment with num_workers=4 machine_type=5 machine_id=2 normalize=$1 normalize_raw=$2 -m student2.cp.jku.at:27017:dcase2020_task2
-OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=$3 python -m experiments.baseline_experiment with num_workers=4 machine_type=5 machine_id=4 normalize=$1 normalize_raw=$2 -m student2.cp.jku.at:27017:dcase2020_task2
-OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=$3 python -m experiments.baseline_experiment with num_workers=4 machine_type=5 machine_id=6 normalize=$1 normalize_raw=$2 -m student2.cp.jku.at:27017:dcase2020_task2
+OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=$2 python -m experiments.baseline_experiment with num_workers=4 machine_type=4 machine_id=1 descriptor=$1 -m student2.cp.jku.at:27017:dcase2020_2
+OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=$2 python -m experiments.baseline_experiment with num_workers=4 machine_type=4 machine_id=2 descriptor=$1 -m student2.cp.jku.at:27017:dcase2020_2
+OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=$2 python -m experiments.baseline_experiment with num_workers=4 machine_type=4 machine_id=3 descriptor=$1 -m student2.cp.jku.at:27017:dcase2020_2
 
+OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=$2 python -m experiments.baseline_experiment with num_workers=4 machine_type=5 machine_id=0 descriptor=$1 -m student2.cp.jku.at:27017:dcase2020_2
+OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=$2 python -m experiments.baseline_experiment with num_workers=4 machine_type=5 machine_id=2 descriptor=$1 -m student2.cp.jku.at:27017:dcase2020_2
+OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=$2 python -m experiments.baseline_experiment with num_workers=4 machine_type=5 machine_id=4 descriptor=$1 -m student2.cp.jku.at:27017:dcase2020_2
+OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=$2 python -m experiments.baseline_experiment with num_workers=4 machine_type=5 machine_id=6 descriptor=$1 -m student2.cp.jku.at:27017:dcase2020_2
