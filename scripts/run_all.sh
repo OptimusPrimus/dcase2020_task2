74ce56bf71bfe17a8790b95a6309e3fc5137f4f9
Paul Primus <primus_paul@yahoo.de>
2020-05-02 15:14:26 +0200

    add dcase baseline

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -5,7 +5,7 @@ conda activate dcase2020_task2
 
 OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=$2 python -m experiments.$1 with num_workers=4 machine_type=0 machine_id=0  -m student2.cp.jku.at:27017:dcase2020_2
 OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=$2 python -m experiments.$1 with num_workers=4 machine_type=0 machine_id=2  -m student2.cp.jku.at:27017:dcase2020_2
-OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=$2 python -m experiments.$1 with num_workers=4 machine_type=0 machine_id=4  -m student2.cp.jku.at:27017:dcase2020_2
+OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=$2 python -m experiments[].$1 with num_workers=4 machine_type=0 machine_id=4  -m student2.cp.jku.at:27017:dcase2020_2
 OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=$2 python -m experiments.$1 with num_workers=4 machine_type=0 machine_id=6  -m student2.cp.jku.at:27017:dcase2020_2
 
 OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=$2 python -m experiments.$1 with num_workers=4 machine_type=1 machine_id=0  -m student2.cp.jku.at:27017:dcase2020_2
