0042555cf54d0cf0da3c3e44a456a65159fa6e7d
Paul Primus <primus_paul@yahoo.de>
2020-06-09 01:50:34 +0200

    add RK3 traning script

--- scripts/convolutional_classification/convolutional_complement_rk0.sh
+++ scripts/convolutional_classification/convolutional_complement_rk0.sh
@@ -14,16 +14,16 @@ wait
 
 wait
 
-./scripts/run_all.sh classification_experiment 0 "debug=False num_hidden=4 hidden_size=512 batch_size=512 learning_rate=1e-4 weight_decay=0 model_class=dcase2020_task2.models.CNN loss_class=dcase2020_task2.losses.BCE -m student2.cp.jku.at:27017:dcase2020_task2_conv_complement_classification_gridsearch" &
-./scripts/run_all.sh classification_experiment 1 "debug=False num_hidden=4 hidden_size=512 batch_size=512 learning_rate=1e-5 weight_decay=0 model_class=dcase2020_task2.models.CNN loss_class=dcase2020_task2.losses.BCE -m student2.cp.jku.at:27017:dcase2020_task2_conv_complement_classification_gridsearch" &
-./scripts/run_all.sh classification_experiment 2 "debug=False num_hidden=4 hidden_size=512 batch_size=512 learning_rate=1e-4 weight_decay=1e-5 model_class=dcase2020_task2.models.CNN loss_class=dcase2020_task2.losses.BCE -m student2.cp.jku.at:27017:dcase2020_task2_conv_complement_classification_gridsearch" &
-./scripts/run_all.sh classification_experiment 3 "debug=False num_hidden=4 hidden_size=512 batch_size=512 learning_rate=1e-5 weight_decay=1e-5 model_class=dcase2020_task2.models.CNN loss_class=dcase2020_task2.losses.BCE -m student2.cp.jku.at:27017:dcase2020_task2_conv_complement_classification_gridsearch" &
-
-wait
-
 ./scripts/run_all.sh classification_experiment 0 "debug=False num_hidden=4 hidden_size=256 batch_size=512 learning_rate=1e-4 weight_decay=0 model_class=dcase2020_task2.models.CNN loss_class=dcase2020_task2.losses.BCE -m student2.cp.jku.at:27017:dcase2020_task2_conv_complement_classification_gridsearch" &
 ./scripts/run_all.sh classification_experiment 1 "debug=False num_hidden=4 hidden_size=256 batch_size=512 learning_rate=1e-5 weight_decay=0 model_class=dcase2020_task2.models.CNN loss_class=dcase2020_task2.losses.BCE -m student2.cp.jku.at:27017:dcase2020_task2_conv_complement_classification_gridsearch" &
 ./scripts/run_all.sh classification_experiment 2 "debug=False num_hidden=4 hidden_size=256 batch_size=512 learning_rate=1e-4 weight_decay=1e-5 model_class=dcase2020_task2.models.CNN loss_class=dcase2020_task2.losses.BCE -m student2.cp.jku.at:27017:dcase2020_task2_conv_complement_classification_gridsearch" &
 ./scripts/run_all.sh classification_experiment 3 "debug=False num_hidden=4 hidden_size=256 batch_size=512 learning_rate=1e-5 weight_decay=1e-5 model_class=dcase2020_task2.models.CNN loss_class=dcase2020_task2.losses.BCE -m student2.cp.jku.at:27017:dcase2020_task2_conv_complement_classification_gridsearch" &
+
+wait
+
+./scripts/run_all.sh classification_experiment 0 "debug=False num_hidden=4 hidden_size=512 batch_size=512 learning_rate=1e-4 weight_decay=0 model_class=dcase2020_task2.models.CNN loss_class=dcase2020_task2.losses.BCE -m student2.cp.jku.at:27017:dcase2020_task2_conv_complement_classification_gridsearch" &
+./scripts/run_all.sh classification_experiment 1 "debug=False num_hidden=4 hidden_size=512 batch_size=512 learning_rate=1e-5 weight_decay=0 model_class=dcase2020_task2.models.CNN loss_class=dcase2020_task2.losses.BCE -m student2.cp.jku.at:27017:dcase2020_task2_conv_complement_classification_gridsearch" &
+./scripts/run_all.sh classification_experiment 2 "debug=False num_hidden=4 hidden_size=512 batch_size=512 learning_rate=1e-4 weight_decay=1e-5 model_class=dcase2020_task2.models.CNN loss_class=dcase2020_task2.losses.BCE -m student2.cp.jku.at:27017:dcase2020_task2_conv_complement_classification_gridsearch" &
+./scripts/run_all.sh classification_experiment 3 "debug=False num_hidden=4 hidden_size=512 batch_size=512 learning_rate=1e-5 weight_decay=1e-5 model_class=dcase2020_task2.models.CNN loss_class=dcase2020_task2.losses.BCE -m student2.cp.jku.at:27017:dcase2020_task2_conv_complement_classification_gridsearch" &
 
 wait
