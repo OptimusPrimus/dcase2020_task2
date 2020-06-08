44344105a48dd7aedc3da073bbf9ee2cc7f64900
Paul Primus <primus_paul@yahoo.de>
2020-06-09 01:39:35 +0200

    add RK3 traning script

--- dcase2020_task2/models/classifier.py
+++ dcase2020_task2/models/classifier.py
@@ -24,7 +24,7 @@ class FCNN(torch.nn.Module):
         activation_fn = activation_dict[activation]
         self.input_shape = input_shape
 
-        sizes = [np.prod(input_shape)] + [hidden_size for _ in range(num_hidden)] + [num_outputs]
+        sizes = [np.prod(input_shape)] + [hidden_size for i in range(num_hidden)] + [num_outputs]
         layers = []
         for i, o in zip(sizes[:-1], sizes[1:]):
             layers.append(torch.nn.Linear(i, o))
@@ -46,4 +46,45 @@ class FCNN(torch.nn.Module):
         x = batch['observations']
         x = x.view(x.shape[0], -1)
         batch['scores'] = self.clf(x)
+        return batch
+
+
+class CNN(torch.nn.Module):
+
+    def __init__(
+            self,
+            input_shape,
+            hidden_size=256,
+            num_hidden=3,
+            num_outputs=1,
+            activation='relu',
+            batch_norm=False,
+            dropout_probability=0.1
+    ):
+        super().__init__()
+
+        activation_fn = activation_dict[activation]
+        self.input_shape = input_shape
+
+        sizes = [input_shape[0]] + [hidden_size for i in range(num_hidden)] + [num_outputs]
+        layers = []
+        for i, o in zip(sizes[:-1], sizes[1:]):
+            layers.append(torch.nn.Conv2d(i, o, kernel_size=(3, 3), stride=2, padding=(1, 1)))
+            # layers.append(torch.nn.Dropout(p=dropout_probability))
+            layers.append(activation_fn())
+
+        _ = layers.pop()
+        layers.append(torch.nn.AdaptiveMaxPool2d(1))
+        # _ = layers.pop()
+
+
+        if batch_norm:
+            _ = layers.pop()
+
+        self.clf = torch.nn.Sequential(*layers)
+        self.apply(init_weights)
+
+    def forward(self, batch):
+        x = batch['observations']
+        batch['scores'] = self.clf(x).view(-1, 1)
         return batch
