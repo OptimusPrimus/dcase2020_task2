b59057cad6687d519a97db40dad414b9b9ad2340
Paul <paul.primus@jku.at>
2020-06-10 16:16:02 +0200

    add

--- scripts/run_and_create_submission_for_per_type_models.sh
+++ scripts/run_and_create_submission_for_per_type_models.sh
@@ -1,6 +1,6 @@
 conda activate dcase2020_task2
 
-OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=0 python -m dcase2020_task2.experiments.$1 with num_workers=4 machine_type=0 machine_id=-1 $2 > /dev/null 2>&1 &
+OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=0 python -m dcase2020_task2.experiments.$1 with num_workers=4 machine_type=0 machine_id=-1 $2 &
 OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=1 python -m dcase2020_task2.experiments.$1 with num_workers=4 machine_type=1 machine_id=-1 $2 > /dev/null 2>&1 &
 OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=2 python -m dcase2020_task2.experiments.$1 with num_workers=4 machine_type=2 machine_id=-1 $2 > /dev/null 2>&1 &
 OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=3 python -m dcase2020_task2.experiments.$1 with num_workers=4 machine_type=3 machine_id=-1 $2 > /dev/null 2>&1 &
