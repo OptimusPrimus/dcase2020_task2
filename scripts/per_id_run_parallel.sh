5a8e508f36ac1dcaf91edc29f08e1ebed05d5179
Paul <paul.primus@jku.at>
2020-06-13 22:30:14 +0200

    add ResNet grid search

--- scripts/per_id_run_parallel.sh
+++ scripts/per_id_run_parallel.sh
@@ -61,3 +61,4 @@ OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=2 python -m dcase2020_task2.experiments.$
 OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=3 python -m dcase2020_task2.experiments.$1 with num_workers=4 machine_type=5 machine_id=5 $2 > /dev/null 2>&1 &
 wait
 OMP_NUM_THREADS=1 CUDA_VISIBLE_DEVICES=0 python -m dcase2020_task2.experiments.$1 with num_workers=4 machine_type=5 machine_id=6 $2 > /dev/null 2>&1 &
+wait
