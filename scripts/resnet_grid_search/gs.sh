666564d16e15140be368da1c2d74f563be8443be
Paul <paul.primus@jku.at>
2020-06-14 18:19:01 +0200

    fix issue with different file lengths

--- scripts/resnet_grid_search/gs.sh
+++ scripts/resnet_grid_search/gs.sh
@@ -6,8 +6,17 @@ for rf in a_bit_larger
 do
   for valid_types in loose
   do
-    for learning_rate_decay in 0.98 0.97 0.96
-      python -m dcase2020_task2.experiments.classification_experiment with learning_rate_decay=$learning_rate_decay machine_type=$machine_type machine_id=$machine_id epochs=$epochs rf=$rf valid_types=$valid_types -m student2.cp.jku.at:27017:debug_
+    for learning_rate_decay in 0.97
+    do
+      for hidden_size in 128 256
+      do
+        python -m dcase2020_task2.experiments.classification_experiment with learning_rate_decay=$learning_rate_decay machine_type=0 machine_id=0 epochs=$epochs rf=$rf valid_types=$valid_types hidden_size=$hidden_size -m student2.cp.jku.at:27017:debug_
+        python -m dcase2020_task2.experiments.classification_experiment with learning_rate_decay=$learning_rate_decay machine_type=1 machine_id=0 epochs=$epochs rf=$rf valid_types=$valid_types hidden_size=$hidden_size -m student2.cp.jku.at:27017:debug_
+        python -m dcase2020_task2.experiments.classification_experiment with learning_rate_decay=$learning_rate_decay machine_type=2 machine_id=0 epochs=$epochs rf=$rf valid_types=$valid_types hidden_size=$hidden_size -m student2.cp.jku.at:27017:debug_
+        python -m dcase2020_task2.experiments.classification_experiment with learning_rate_decay=$learning_rate_decay machine_type=3 machine_id=1 epochs=$epochs rf=$rf valid_types=$valid_types hidden_size=$hidden_size -m student2.cp.jku.at:27017:debug_
+        python -m dcase2020_task2.experiments.classification_experiment with learning_rate_decay=$learning_rate_decay machine_type=4 machine_id=1 epochs=$epochs rf=$rf valid_types=$valid_types hidden_size=$hidden_size -m student2.cp.jku.at:27017:debug_
+        python -m dcase2020_task2.experiments.classification_experiment with learning_rate_decay=$learning_rate_decay machine_type=5 machine_id=0 epochs=$epochs rf=$rf valid_types=$valid_types hidden_size=$hidden_size -m student2.cp.jku.at:27017:debug_
+      done
     done
   done
 done
